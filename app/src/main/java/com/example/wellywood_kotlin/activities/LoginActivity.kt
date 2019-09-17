8086b3e48b7cd9f17d82cf6d8b26bd9ed21188e4
bondkyal <bondkyal@vuw.ac.nz>
2019-09-17 18:04:13 +1200

    Add login functionality

--- app/src/main/java/com/example/wellywood_kotlin/activities/LoginActivity.kt
+++ app/src/main/java/com/example/wellywood_kotlin/activities/LoginActivity.kt
@@ -1,10 +1,13 @@
 package com.example.wellywood_kotlin.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.wellywood_kotlin.R
+import com.example.wellywood_kotlin.services.FirebaseService
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,9 +15,22 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val emailField: EditText = findViewById(R.id.email)
+        val passField: EditText = findViewById(R.id.password)
+
+
         val btnLogin: Button = findViewById(R.id.buttonLogin)
         btnLogin.setOnClickListener {
-            Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, emailField.editableText.toString(), Toast.LENGTH_SHORT).show()
+            val success: Boolean = FirebaseService.login(
+                emailField.editableText.toString(), passField.editableText.toString())
+
+            if (success) {
+                Toast.makeText(this, "Successfully logged in!", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, HomeActivity::class.java))
+            } else {
+                Toast.makeText(this, "Failed to login", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
--- app/src/main/java/com/example/wellywood_kotlin/services/FirebaseService.kt
+++ app/src/main/java/com/example/wellywood_kotlin/services/FirebaseService.kt
@@ -1,6 +1,31 @@
 package com.example.wellywood_kotlin.services
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import java.lang.Exception
+
 object FirebaseService {
 
+    fun login(email: String, pass: String): Boolean {
+        val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+        try {
+            val task: Task<AuthResult> = mAuth.signInWithEmailAndPassword(email, pass)
+            task.addOnCompleteListener(OnCompleteListener {
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Successfully logged in")
+                } else {
+                    Log.w(TAG, "Failed to login", task.exception)
+                }
+            })
+        } catch (e: Exception) {
+            println(e)
+            return false
+        }
+        return true
+    }
 
 }
